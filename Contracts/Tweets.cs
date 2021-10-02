37ee101971b1c1262227cba5ef0df2961a4e9ce7
varad <varadsoham555@gmail.com>
2021-10-02 20:15:08 +0530

    initial commit

---
+++ Contracts/Tweets.cs
@@ -0,0 +1,32 @@
+﻿namespace TwitterAPI.Contracts
+{
+
+    public class Tweets
+    {
+        public Datum[] data { get; set; }
+        public Includes includes { get; set; }
+    }
+
+    public class Includes
+    {
+        public User[] users { get; set; }
+    }
+
+    public class User
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public DateTime created_at { get; set; }
+        public string username { get; set; }
+    }
+
+    public class Datum
+    {
+        public string text { get; set; }
+        public string id { get; set; }
+        public string author_id { get; set; }
+        public DateTime created_at { get; set; }
+    }
+
+
+}
---
+++ Controllers/TwitterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using TwitterAPI.Contracts;
+
+namespace TwitterAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class TwitterController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _configuration;
+        public TwitterController(IHttpClientFactory clientFactory, IConfiguration configuration)
+        {
+            _clientFactory = clientFactory;
+            _configuration = configuration;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTweet(string id)
+        {
+            var bearerToken = _configuration["BearerToken"];
+            var client = _clientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization =
+               new AuthenticationHeaderValue("Bearer", bearerToken);
+            var posts =await  client.GetAsync($"https://api.twitter.com/2/tweets?ids={id}&tweet.fields=created_at&expansions=author_id&user.fields=created_at");
+            var desPosts = JsonSerializer.Deserialize<Tweets>(await posts.Content.ReadAsStringAsync());
+            return Ok(desPosts);
+        }
+    }
+}
